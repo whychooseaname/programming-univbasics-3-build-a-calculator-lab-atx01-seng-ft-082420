b4ea86b0f23ccfdcfd60abad3a7a7aa5ffcdbc54
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-03 23:48:56 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,21 +3,21 @@ sum=num1+num2
 end
 
 def subtraction(num1, num2)
-sum=num1-num2
+answer=num1-num2
 end
 
 def division(num1, num2)
-sum=num1/num2
+answer=num1/num2
 end
 
 def multiplication(num1, num2)
-sum=num1*num2
+answer=num1*num2
 end
 
 def modulo(num1, num2)
-sum=num1%num2
+answer=num1%num2
 end
 
 def square_root(num)
-sum=sqrt(num)
+answer=sqrt(num)
 end

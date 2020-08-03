f5f1049761b5c9d3e4905f1d2fe6219b08b99eb7
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-03 23:37:13 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -7,17 +7,17 @@ sum=num1-num2
 end
 
 def division(num1, num2)
-
+sum=num1/num2
 end
 
 def multiplication(num1, num2)
-
+sum=num1*num2
 end
 
 def modulo(num1, num2)
-
+sum=num1%num2
 end
 
 def square_root(num)
-
+sum=num**2
 end
